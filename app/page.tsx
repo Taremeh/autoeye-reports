8dc60e48d9553bd99bbb26b2699ec1fbcd109beb
Tarek <tarekalakmeh@hotmail.ch>
2025-02-08 19:11:45 +0100

    EEG images

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import { BlogPosts } from 'app/components/posts'
+import Image from 'next/image'
+import fs from 'fs'
+import path from 'path'
 
 export default function Page() {
+  const imagesDir = path.join(process.cwd(), '/public/images')
+  const imageFiles = fs.readdirSync(imagesDir).filter(file => file.endsWith('_alpha_power_hard.gif'))
+  // const imageFiles = fs.readdirSync(imagesDir).filter(file => file.endsWith('_alpha_progression_hard.png'))
+  
+  // only keep first three elements of imageFiles
+  //imageFiles.length = 3
+
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        CC-EEG Individual Reports
       </h1>
       <p className="mb-4">
         {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
@@ -15,6 +25,20 @@ export default function Page() {
       </p>
       <div className="my-8">
         <BlogPosts />
+      </div>
+      <div className="grid grid-cols-5 gap-4">
+        {imageFiles.map((file) => (
+          <div key={file} className="relative w-full h-44">
+            <Image
+              src={`/images/${file}`}
+              alt={file}
+              layout="fill"
+              objectFit="cover"
+              // width={300}
+              // height={300}
+            />
+          </div>
+        ))}
       </div>
     </section>
   )
